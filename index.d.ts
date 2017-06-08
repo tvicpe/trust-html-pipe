2908263c91b003535daa3bcb3509bd53ae7520e8
tvicpe <tv@tomsa.md>
2017-06-08 13:35:20 +0100

    update index.d.ts file increase the version to 0.0.6

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,5 @@
-// Type definitions for TrustHtmlPipe
-// Project: TrustHtmlPipe
+// Type definitions for trust-html-pipe
+// Project: trust-html-pipe
 // Definitions by: tomsa.md
 
-/*~ If this module is a UMD module that exposes a global variable 'myClassLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-
-import { DomSanitizer } from "@angular/platform-browser";
-
-// export as namespace TrustHtmlPipe;
-
-/*~ This declaration specifies that the class constructor function
- *~ is the exported object from the file
- */
-
-/*~ If you want to expose types from your module as well, you can
- *~ place them in this block.
- */
-
-declare class TrustHtmlPipe {
-  constructor(sanitizer: DomSanitizer);
-
-  public transform(htmlString: string): any;
-}
-
-export {TrustHtmlPipe};
+export { TrustHtmlPipe } from './src/trust-html.pipe';
